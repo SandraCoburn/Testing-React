e85b7fd6fdec7db6edc15783c001e8a5bac4182a
Sandra Coburn <sandra_coburn@hotmail.com>
2020-01-27 16:46:33 -0800

    mvp complete

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { render, fireEvent, wait, getByTestId } from "@testing-library/react";
+import axios from "axios";
+import { render, fireEvent, waitForDomChange } from "@testing-library/react";
 import StarWarsCharacters from "./StarWarsCharacters";
 import { getData as mockGetData } from "../api";
+// import { renderHook } from "@testing-library/react-hooks";
 
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-import Axios from "axios";
+// import Axios from "axios";
 
 jest.mock("../api");
 
-test("Renders the characters component", async () => {
+test("Renders the characters names", async () => {
   mockGetData.mockResolvedValueOnce({ count: 87 });
 
   const { getByText } = render(<StarWarsCharacters />);
@@ -23,16 +25,28 @@ test("Renders the characters component", async () => {
   fireEvent.click(nextButton);
 
   //   expect(mockGetData).toHaveBeenCalledTimes(1);
-  //   expect(mockGetData).toHaveBeenCalledWith(URL);
+  //   expect(mockGetData).toHaveBeenCalledWith();
+});
+test("Testing async with api call", async () => {
+  mockGetData.mockResolvedValue({
+    results: [
+      {
+        name: "Luke Skywalker",
+        height: "172",
+        mass: "77",
+        id: Date.now()
+      }
+    ]
+  });
+
+  expect(mockGetData).toHaveBeenCalledTimes(0);
+  const { getByText } = render(<StarWarsCharacters />);
+
+  await (() => expect(getByText(/Luke/i).toBeInDocument()));
 });
 
 // test("Test the next button is not disabled", () => {
 //   expect(getByTestId("button")).not.toBeDisabled();
-// });
-
-// test("Do buttons have style?", () => {
-//   const button = getByTestId("button:disabled");
-//   expect(button).toHaveStyle("cursor: now-allowed");
 // });
 
 test("test test", () => {});
